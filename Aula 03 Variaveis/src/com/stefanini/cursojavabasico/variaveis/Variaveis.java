21f414484c149f1cbbb7ca7a9bc8532cf55d7459
eduar <eduar@Eduardo>
2022-05-27 10:02:49 -0300

    Feito.

---
+++ Aula 03 Variaveis/src/com/stefanini/cursojavabasico/variaveis/Variaveis.java
@@ -0,0 +1,31 @@
+package com.stefanini.cursojavabasico.variaveis;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		byte numByte = 127; 
+		short numShort = 12321;
+		int numInt = 2147483647;
+		long numLong = 2147483647;
+
+		Float numFloat = 10.000f; 
+		double numDouble = 10000000000000.000;
+		char varChar = 'F';
+		boolean varBoolean = true;
+
+
+		String texto = "Bem vindos Devs!";
+		Integer numeroConta = 4000;
+		Long numeroLongo = 1213123L;
+
+		String nomeCompleto = "Devide";
+
+
+		}
+
+	
+
+
+	}
+
