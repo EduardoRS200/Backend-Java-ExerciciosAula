fca1b04e8f0911c1b12cdedbb31fd46d3317f4e3
eduar <eduar@Eduardo>
2022-06-05 22:44:28 -0300

    POO - Pratica 3

---
+++ Aula 11/src/br/com/stefanini/orientacaoaobjetos/BMW.java
@@ -0,0 +1,16 @@
+package br.com.stefanini.orientacaoaobjetos;
+
+public class BMW extends Carro {
+
+	public BMW() {
+		super();
+	}
+	
+	
+	@Override
+	public double calcularTaxaAceleracao(double velocidadeFinal, double tempoFinal) {
+		System.out.println("BMW sobrescrita do metodo...");
+		return (velocidadeFinal / tempoFinal); 
+	}
+
+}
