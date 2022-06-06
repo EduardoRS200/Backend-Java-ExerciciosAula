50d9498352d4b77c72f3847b9eb0ad169a0ab076
eduar <eduar@Eduardo>
2022-06-05 21:38:39 -0300

    POO - Pratica 1

---
+++ Aula 09/src/br/com/stefanini/orientacaoaobjetos/Carro.java
@@ -0,0 +1,20 @@
+package br.com.stefanini.orientacaoaobjetos;
+
+public class Carro {
+
+	String marca;
+	String modelo;
+	int velocidadeMaxima;
+	int taxaAceleracao;
+	double quilometrosPorLitro;
+
+	void acelerar () {
+		System.out.println("Acelerando..");
+	}
+			
+	void frear() {
+		System.out.println("Freando..");
+	}
+
+
+}
