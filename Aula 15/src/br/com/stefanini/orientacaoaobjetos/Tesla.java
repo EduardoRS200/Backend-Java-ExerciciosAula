3383879a248f7b28e6b2e71bb872a4af473f7841
eduar <eduar@10.2.48.12>
2022-06-12 16:55:37 -0300

    Exceptions

---
+++ Aula 15/src/br/com/stefanini/orientacaoaobjetos/Tesla.java
@@ -0,0 +1,26 @@
+package br.com.stefanini.orientacaoaobjetos;
+
+public class  Tesla extends Carro {
+	
+	private boolean dirigeSozinho;
+	
+	public Tesla() {
+		super();
+	}
+	
+	@Override
+	public double calcularTaxaAceleracao(double velocidadeFinal, double tempoFinal) {
+		System.out.println("Tesla, sobrescrita do metodo...");
+		return (velocidadeFinal * tempoFinal); 
+	}
+
+	
+	public boolean  isDirigeSozinho() {
+		return dirigeSozinho;
+	}
+
+	public void setDirigeSozinho(boolean dirigeSozinho) {
+		this.dirigeSozinho = dirigeSozinho;
+	}
+
+}
